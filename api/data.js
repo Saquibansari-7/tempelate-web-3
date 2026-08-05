4eb2a710ac1efafe3b3f3af436b2b1ade5ee886a
Saquibansari-7 <saquib6111@gmail.com>
2026-08-05 14:38:31 +0530

    copilo2

--- api/data.js
+++ api/data.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = (req, res) => {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default (req, res) => {
   try {
     const distPath = path.join(__dirname, '..', 'dist', 'data.json');
     const rootPath = path.join(__dirname, '..', 'data.json');
